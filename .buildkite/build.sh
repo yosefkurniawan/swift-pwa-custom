d051f5b50d57be64adc6e8e3f673638258f00772
andycungkrinx <andy.setiawan@sirclo.com>
2022-10-18 17:43:34 +0700

    fixing builder

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -10,6 +10,6 @@ githead=$(git rev-parse HEAD)
 set -ex
 echo "Building Backend Docker image with tag " $$githead-$$ts
 echo $githead-$ts | buildkite-agent meta-data set "image-tag" 
-docker login -u _json_key -p "$(cat /etc/service_key/service-account-key.json | tr '\n' ' ')" https://asia.gcr.io.
+docker login -u _json_key -p "$(cat /etc/service_key/service-account-key.json | tr '\n' ' ')" https://asia.gcr.io
 docker build -t asia.gcr.io/sirclo-iii-nonprod/$1-dev:$githead-$ts -f Dockerfile .
 docker push asia.gcr.io/sirclo-iii-nonprod/$1-dev:$githead-$ts
